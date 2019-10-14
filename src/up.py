a17c1bba44860bfb492e5367c758e5290b8a0005
guochen <chenguo@jieyuechina.com>
2019-10-14 15:35:23 +0800

    service log

--- src/up.py
+++ src/up.py
@@ -1,11 +1,37 @@
 import socket
+import logging
+import logging.handlers
 
-HOST = '192.168.56.101'
-PORT = 9999
+class Service(object):
+    def __init__(self, name, host, port, log_level=logging.DEBUG):
+        self.host = host
+        self.port = port
+        self.logger = logging.getLogger(name)
+        fh = logging.handlers.TimedRotatingFileHandler(name + '.log', "D", 1, 10)
+        fh.setFormatter(logging.Formatter('%(asctime)s %(filename)s_%(lineno)d: [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S'))
+        self.logger.addHandler(fh)
+        self.logger.setLevel(log_level)
 
-udp_server_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-udp_server_sock.bind((HOST, PORT))
+    def start(self):
+        udp_server_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        udp_server_sock.bind((self.host, self.port))
 
-while True:
-    msg, addr = udp_server_sock.recvfrom(1024)
-    udp_server_sock.sendto(str(addr).encode('utf-8') + msg,addr)
+        self.logger.debug('service started....')
+
+        while True:
+            req, addr = udp_server_sock.recvfrom(1024)
+            self.logger.debug('[REQ]' + str(addr) + ': ' + req.decode('utf-8'))
+            udp_server_sock.sendto(self._reply(addr, req).encode('utf-8'), addr)
+
+    def _reply(self, *req):
+        res = ''
+        for i in req:
+            res = res + ' ' + str(i)
+        return res.strip()
+
+
+if '__main__' == __name__:
+    HOST = '192.168.56.101'
+    PORT = 9999
+    serv = Service(name='sample', host=HOST, port=PORT)
+    serv.start()
