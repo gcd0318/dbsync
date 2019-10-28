e64c19b711eece81ac3922cafb11b7fd1d29dec1
guochen <chenguo@jieyuechina.com>
2019-10-28 13:25:10 +0800

    new ips definition

--- src/base/cluster.py
+++ src/base/cluster.py
@@ -1,11 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+import sys
+sys.path.append(os.path.abspath('..'))
+
+from config import Config
+
 class Client(object):
     def __init__(self, server_ip):
         self.server = server_ip
 
 class Cluster(object):
-    def __init__(self, nodes=[]):
-        self.nodes = nodes
+    def __init__(self, node_ips=[], conf_fn='../db.conf'):
+        self.node_ips = node_ips
+        if not node_ips:
+            conf = Config(conf_fn).read_data()
+            self.node_ips = conf.get('ips', '').split()
 
+    '''
     def all_status(self, data):
         threads = []
         def runner(anode):
@@ -18,21 +33,24 @@ class Cluster(object):
         for t in threads:
             t.join()
         return data
+    '''
 
-    def add_node(self, anode):
-        if anode.is_alive():
-            self.nodes.append(anode)
+    def add_node(self, node_ip):
+        self.node_ips.append(node_ip)
 
-    def remove_node(self, anode):
-        nodes = []
-        for node in self.nodes:
-            if node.ip != anode.ip:
-                nodes.append(node)
-        self.nodes = nodes
+    def remove_node(self, node_ip):
+        node_ips = []
+        for ip in self.node_ips:
+            if node_ip != ip:
+                nodes.append(node_ip)
+        self.node_ips = node_ips
 
-    def exec_sql(self, sql):
-        for node in nodes:
-            node.db.exec(sql)
+#    def exec_sql(self, sql):
+#        for node in nodes:
+#            node.db.exec(sql)
 
 if '__main__' == __name__:
     c = Cluster()
+    print(c.node_ips)
+    c.add_node('127.0.0.1')
+    print(c.node_ips)
