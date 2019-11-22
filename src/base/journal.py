717b74fed36f9284829431c70d36a25e783285a8
guochen <chenguo@jieyuechina.com>
2019-11-22 17:22:02 +0800

    journal init

--- src/base/journal.py
+++ src/base/journal.py
@@ -1,1 +1,17 @@
+import time
+
+from base import SEP
+
 class Journal(object):
+    def __init__(self, jf_path):
+        self.jf_path = jf_path
+
+    def to_journal(self, msg):
+        with open(self.jf_path, 'a') as f:
+            print(time.localtime(), SEP, msg, file=f)
+
+    def replay(self):
+        with open(self.jf_path, 'r') as f:
+            line = f.readline()
+            while line is not None:
+                exec(line.split(SEP, 1))
